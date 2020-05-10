c116a719e3b2b5a85b273ee750636db158bd32c4
Colby <colbykrawczyk10@gmail.com>
2020-05-09 22:56:43 -0400

    Initial Commit

---
+++ src/main/java/net/elytrapvp/elytracore/ElytraCore.java
@@ -0,0 +1,20 @@
+package net.elytrapvp.elytracore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElytraCore extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+}
