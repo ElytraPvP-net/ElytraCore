47518198cb3247e64ae34cdffa6afa617b9160dd
Colby <colbykrawczyk10@gmail.com>
2020-05-30 04:16:46 -0400

    Hide Vanished Players on join

--- src/main/java/net/elytrapvp/elytracore/listeners/PlayerJoinListener.java
+++ src/main/java/net/elytrapvp/elytracore/listeners/PlayerJoinListener.java
@@ -2,9 +2,13 @@ package net.elytrapvp.elytracore.listeners;
 
 import net.elytrapvp.elytracore.ElytraSettings;
 import net.elytrapvp.elytracore.chat.Message;
+import net.elytrapvp.elytracore.commands.VanishCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.UUID;
 
 public class PlayerJoinListener implements Listener
 {
@@ -21,6 +25,11 @@ public class PlayerJoinListener implements Listener
         }
 
         e.setJoinMessage(Message.translate("&2[&a+&2] &f") + e.getPlayer().getDisplayName());
+
+        for(UUID u : VanishCommand.getVanished())
+        {
+            e.getPlayer().hidePlayer(Bukkit.getPlayer(u));
+        }
     }
 
 }
