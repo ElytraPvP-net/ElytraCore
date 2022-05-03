450cd34db265448695806795aad8dc4129ef37eb
colby <colbykrawczyk10@gmail.com>
2022-05-03 02:57:07 -0400

    Made the /workbench command functional

--- src/main/java/net/elytrapvp/elytracore/misc/commands/WorkbenchCMD.java
+++ src/main/java/net/elytrapvp/elytracore/misc/commands/WorkbenchCMD.java
@@ -1,4 +1,29 @@
 package net.elytrapvp.elytracore.misc.commands;
 
-public class WorkbenchCMD {
+import net.elytrapvp.elytracore.utilities.commands.AbstractCommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * This class runs the workbench command, which opens a virtual crafting table.
+ */
+public class WorkbenchCMD extends AbstractCommand {
+
+    /**
+     * Creates the /workbench command with the permission "elytracore.workbench".
+     */
+    public WorkbenchCMD() {
+        super("workbench", "elytracore.workbench", false);
+    }
+
+    /**
+     * This is the code that runs when the command is sent.
+     * @param sender The player (or console) that sent the command.
+     * @param args The arguments of the command.
+     */
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        Player player = (Player) sender;
+        player.openWorkbench(player.getLocation(), true);
+    }
 }
